71faf82bb167d9dd208df2e8ecc79e4bf51e0aec
unknown <Alexander Kulpin>
2023-05-27 20:07:15 +0300

    object Color Wheel

--- script/ColorMixer.js
+++ script/ColorMixer.js
@@ -1,45 +1,58 @@
-// alert('script start');
+alert('script start');
 
-const colorWheel = document.getElementById('ColorWheel');
-const wheelHandle = document.getElementById('WheelHandle');
-const smallCircle = document.getElementById('SmallCircle');
-const bigCircle = document.getElementById('BigCircle');
-const conicGradient = document.getElementById('ConicGradient');
+const ColorWheel = {
+    started: false,
+    element: null,
+    rect: null,
+    slider: {
+        started: false,
+        element: null,
+        rect: null,
+        active:false,
 
-//conicGradient.onmousemove = gradientMouseMove;
-//smallCircle.onmousedown = smallCircleDown;
-//smallCircle.onmouseup = smallCircleUp;
+        center: {
+            started: false,
+            element: null,
+            start() {
+                if (!this.started) {
+                    this.element = document.getElementById('WheelSliderCenter');
+                    this.element.addEventListener('mousedown', (event)=>{ ColorWheel.centerDown(); })
+                    this.element.addEventListener('mouseup', (event)=>{ ColorWheel.centerUp(); })
+                    this.started = true;
+                }
+            }
+        
+        },
 
-const handleRect = wheelHandle.getBoundingClientRect();
-let wheelRect = colorWheel.getBoundingClientRect();
+        start() {
+            if (!this.started) {
+                this.element = document.getElementById('WheelSlider');
+                this.rect = this.element.getBoundingClientRect();
+                this.center.start();
+                this.started = true;
+            }
+        }
+    },
+    start() {
+        if (!this.started) {
+            this.element = document.getElementById('ColorWheel');
+            this.rect = this.element.getBoundingClientRect();
+            this.slider.start();
+            this.started = true;
+        }
+    },
 
-let handleACtive = false;
-function smallCircleDown(event) {
-    console.log('smallCircleDown');
-    smallCircle.setAttribute('stroke', '#000');
-    handleACtive = true;
-}
+    centerDown() {
+        this.slider.active = true;
+        this.slider.center.element.setAttribute('stroke', '#000');
+    },
 
-function smallCircleUp(event) {
-    smallCircle.setAttribute('stroke', '#FFF');
-    handleACtive = false;
-}
-
-function gradientMouseMove(event) {
-    if (handleACtive) {
-        console.log(`gradientMouseMove ${event.clientX} ${event.clientY}`);
-        wheelHandle.style.left = event.clientX - wheelRect.left - handleRect.width / 2;
-        wheelHandle.style.top = event.clientY - wheelRect.top - handleRect.height / 2;
+    centerUp() {
+        this.slider.active = false;
+        this.slider.center.element.setAttribute('stroke', '#FFF');
     }
-}
+};
 
-function docScroll(event) {
-    wheelRect = colorWheel.getBoundingClientRect();
-}
+ColorWheel.start();
 
-conicGradient.onmousemove = gradientMouseMove;
-document.addEventListener('scroll', docScroll);
-smallCircle.onmousedown = smallCircleDown;
-smallCircle.onmouseup = smallCircleUp;
-
-// alert('script finish');
+alert('script finish');
