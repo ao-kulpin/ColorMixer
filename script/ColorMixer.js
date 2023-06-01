f9c1e0843fc4a1b9a2bcb23f54ad831be6528bff
unknown <Alexander Kulpin>
2023-06-01 13:20:09 +0300

    objects Hue and Master

--- script/ColorMixer.js
+++ script/ColorMixer.js
@@ -4,6 +4,10 @@ const ColorWheel = {
     started: false,
     element: null,
     rect: null,
+    centerX: 0,
+    centerY: 0,
+    currentHue: 0,
+    currentRadius: 0,
     gradient:{
         started: false,
         element: null,
@@ -44,8 +48,6 @@ const ColorWheel = {
             start() {
                 if (!this.started) {
                     this.element = document.getElementById('WheelSliderCenter');
-                    // his.element.addEventListener('mousedown', (event)=>{ColorWheel.sliderActivate();});
-                    //this.element.addEventListener('mouseup', (event)=>{ColorWheel.sliderDeactivate();});
                     this.started = true;
                 }
             }
@@ -63,11 +65,11 @@ const ColorWheel = {
     },
     start() {
         if (!this.started) {
+            this.slider.start();
+            this.gradient.start();
             this.element = document.getElementById('ColorWheel');
             this.rect = this.element.getBoundingClientRect();
             document.addEventListener('scroll', ()=>ColorWheel.onScroll());
-            this.slider.start();
-            this.gradient.start();
             this.started = true;
         }
     },
@@ -90,9 +92,70 @@ const ColorWheel = {
     },
     onScroll() {
         ColorWheel.rect = ColorWheel.element.getBoundingClientRect();
-    }
-};
+    },
+    getViewPoint() {
+        this.rect = this.element.getBoundingClientRect();
+        this.slider.rect = ColorWheel.slider.element.getBoundingClientRect();
 
-ColorWheel.start();
+        this.centerX = (this.rect.left + this.rect.right) / 2;
+        this.centerY = (this.rect.top + this.rect.bottom) / 2;
+    },
+    calcHue(x, y) {
+        if (x === this.centerX && y === this.centerY) {
+            // the slider is on the center - calculating is impossible
+            return;
+        }
+
+        const distX = x - this.centerX;
+        const distY = y - this.centerY;
+
+        this.currentRadius = Math.sqrt(distX * distX + distY * distY);
+
+        const sinHue = (y - this.centerY) / this.currentRadius;
+        const asinHue = Math.asin(sinHue) * 180 / Math.PI;
+        if (asinHue >= 0) {
+            if (x >= this.centerX) {
+                this.currentHue = asinHue;
+            } else {
+                this.currentHue = 180 - asinHue;
+            }
+        } else {
+            if (x >= this.centerX) {
+                this.currentHue = 360 + asinHue;
+            } else {
+                this.currentHue = 180 + asinHue;
+            }
+        }
+    }
+}  // end of ColorWheel
+
+const Hue = {
+    started: false,
+    elementText: null,
+    start() {
+        if (!this.started) {
+            this.elementText = document.getElementById('HueText');
+            this.elementText.addEventListener('input', (event) => Hue.onChangeText());
+            this.started = true;
+        }
+    },
+    onChangeText() {
+
+    }
+}  // end of Hue
+
+const Master = {
+    started: false,
+    start() {
+        if (!this.started) {
+            ColorWheel.start();
+            Hue.start();
+            this.started = true;
+        }
+    }
+
+} // end of Master
+
+Master.start();
 
 // alert('script finish');
